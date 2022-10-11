062b89aaf040ca1cd8e7cc25f456f4e773b24cb4
David Lidebrandt <david.lidebrandt@gmail.com>
2022-10-11 18:39:50 +0200

    Add language icons

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-
-
 window.addEventListener("scroll", handleSectionScroll);
 
-
 function handleSectionScroll() {
-    const sections = document.querySelectorAll(".large-icon");
-    console.log(sections);
-    sections.forEach((currentElement)=> {
-        const windowHeight = window.innerHeight;
-        const currentElementTop = currentElement.getBoundingClientRect().top;
-        if(currentElementTop <= windowHeight) {
-            currentElement.classList.add("large-icon-shown");
-        }
-    })
+  const sections = document.querySelectorAll(".large-icon");
+  sections.forEach((currentElement) => {
+    const windowHeight = window.innerHeight;
+    const currentElementTop = currentElement.getBoundingClientRect().top;
+    if (currentElementTop <= windowHeight) {
+      currentElement.classList.add("large-icon-shown");
+    }
+  });
 }
